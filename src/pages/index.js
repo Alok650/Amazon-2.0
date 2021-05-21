0df6002c9decbdfe15efe54fe1c77c10c410c0fc
Alok650 <alokp650@gmail.com>
2021-05-21 15:35:35 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Header from "../components/Header"
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Amazon 2.0</title>
+      </Head>
+
+      {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
+        <Header/>
+      {/* ---- ---- */}
+    </div>
+  );
+}
